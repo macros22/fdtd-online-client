20c90376fe002f30fb55f7fb1d40c97d103bb608
max99xam <pepetrov123@list.ru>
2022-02-06 00:27:37 +0300

    changed header

--- layout/Header/index.tsx
+++ layout/Header/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { DropdownButton, Navbar, NavDropdown, Nav } from "react-bootstrap";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
-const WAVE_OPTICS_NAME = "ВОЛНОВАЯ ОПТИКА";
+import cn from 'classnames';
+
+const WAVE_OPTICS_NAME = 'ВОЛНОВАЯ ОПТИКА';
 
 import {
   LAB_1_NAME,
@@ -10,31 +11,40 @@ import {
   LAB_3_NAME,
   LAB_4_NAME,
   LAB_5_NAME,
-} from "names/navbar.name";
+} from 'names/navbar.name';
 
 const sections = [
-  { title: LAB_1_NAME, url: "/lab1" },
-  { title: LAB_2_NAME, url: "/lab2" },
-  { title: LAB_3_NAME, url: "/lab3" },
-  { title: LAB_4_NAME, url: "/lab4" },
-  { title: LAB_5_NAME, url: "/lab5" },
+  { title: LAB_1_NAME, url: '/lab1' },
+  { title: LAB_2_NAME, url: '/lab2' },
+  { title: LAB_3_NAME, url: '/lab3' },
+  { title: LAB_4_NAME, url: '/lab4' },
+  { title: LAB_5_NAME, url: '/lab5' },
 ];
 
-import classes from "./header.module.scss";
-import Dropdown from "components/molecules/Dropdown";
+import styles from './header.module.scss';
+import Dropdown from 'components/molecules/Dropdown';
 
 export default function Header(): JSX.Element {
-  const [currentPage, setCurrentPage] = React.useState(0);
+  const currentLabName =
+    'Моделирование явления интерференции электромагнитных волн';
+
+  enum LabContentKind {
+    MODEL = 'MODEL',
+    THEORY = 'THEORY',
+  }
+
+  const [currentLabContent, setCurrentLabContent] =
+    React.useState<LabContentKind>(LabContentKind.MODEL);
 
   return (
     <div>
-      <div className={classes.parentUp}>
+      <div className={styles.parentUp}>
         {/* <Link href="/">
-                    <a className={classes.logo}>Теория</a>
+                    <a className={styles.logo}>Теория</a>
                 </Link> */}
-        <nav className={classes.navbar}>
-          <Link href="/">
-            <a className={classes.logo}>{WAVE_OPTICS_NAME}</a>
+        <nav className={styles.navbar}>
+          <Link href='/'>
+            <a className={styles.logo}>{WAVE_OPTICS_NAME}</a>
           </Link>
 
           {/* <label htmlFor="lab">Выбор лабораторной:</label>
@@ -45,11 +55,37 @@ export default function Header(): JSX.Element {
                         <option value="Gold">Gold</option>
                     </select> */}
 
+          <div className={styles.labContentType}>
+            <Link href='#'>
+              <a
+                onClick={() => setCurrentLabContent(LabContentKind.THEORY)}
+                className={cn({
+                  [styles.activeLabContentType]:
+                    currentLabContent == LabContentKind.THEORY,
+                })}
+              >
+                Теория
+              </a>
+            </Link>
+            <div className={styles.verticalLine}></div>
+            <Link href='#'>
+              <a
+                onClick={() => setCurrentLabContent(LabContentKind.MODEL)}
+                className={cn(styles.labContentType, {
+                  [styles.activeLabContentType]:
+                    currentLabContent == LabContentKind.MODEL,
+                })}
+              >
+                Модель
+              </a>
+            </Link>
+          </div>
+
           <Dropdown />
 
           {/* <Nav>
                         <NavDropdown
-                            // className={classes.dropDownTitle}
+                            // className={styles.dropDownTitle}
                             id="nav-dropdown-dark-example"
                             title="Выбор лабораторной"
                             menuVariant="light"
@@ -57,7 +93,7 @@ export default function Header(): JSX.Element {
                             {sections.map((section, index) => (
                                 <Link key={section.title + index} href={section.url} passHref>
                                     <NavDropdown.Item
-                                    // className={(currentPage == index ? ' active' : '') + ' ' + classes.dropDownItem}
+                                    // className={(currentPage == index ? ' active' : '') + ' ' + styles.dropDownItem}
                                     >
                                         {section.title}
                                     </NavDropdown.Item>
@@ -67,14 +103,8 @@ export default function Header(): JSX.Element {
                     </Nav> */}
         </nav>
       </div>
-      <div className={classes.parent}>
-        <Link href="/">
-          <a className={classes.logo}>Теория</a>
-        </Link>
-        <div className={classes.verticalLine}></div>
-        <Link href="/">
-          <a className={classes.logo}>Модель</a>
-        </Link>
+      <div className={styles.parent}>
+        <h2>{currentLabName}</h2>
       </div>
     </div>
   );
