7a774564dadf0113bc3b0691847c3953da109cdc
max99xam <pepetrov123@list.ru>
2022-02-13 12:38:21 +0300

    changed colors and font size in nav dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-//@ts-ignore
-import TestMDX from 'theory-posts/test.mdx';
 
 export default function Index() {
   return (
     <>
-      <TestMDX />
+      <p>sd</p>
     </>
   );
 }
