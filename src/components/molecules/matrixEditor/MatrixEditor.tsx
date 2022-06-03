ef58b8f3d0b03a3ff8d2b5de4bf283e368248846
max99xam <pepetrov123@list.ru>
2022-06-03 21:59:26 +0300

    added inner dividers to matrix editor

--- src/components/molecules/matrixEditor/MatrixEditor.tsx
+++ src/components/molecules/matrixEditor/MatrixEditor.tsx
@@ -27,6 +27,7 @@ import PreviewMatrix from "./PreviewMatrixEditor";
 import { selectCurrentSimulationDimension } from "store/reducers/app-config.reducer";
 import { colors } from "./colors";
 import Modal from "../modal/Modal";
+import Divider from "components/atoms/divider/Divider";
 
 const gridSizes1D = [5, 50, 100, 200];
 const gridSizes2D = [11, 55, 110, 220];
@@ -73,203 +74,189 @@ const MatrixEditor: React.FC<MatrixEditorProps> = ({
 
   return (
     <>
-      {/* <div className={styles.substrate}></div> */}
-
-      {/* <div className={styles.modalWrapper}> */}
-
-<Modal>
-<div className={styles.nonScrollableContent}> 
-                {/* <p>This short title <b>IS NOT</b> scrollable</p> */}
-                <h2 className={styles.matrixPickerTitle}>
-                    Matrix picker</h2>
-                <h2 className={styles.editorTitle}>
-                    Editor</h2>
-                <h2 className={styles.materialPickerTitle}>
-                    Material picker</h2>
-            </div>
-            <div className={styles.scrollableContent}> 
-        <div className={styles.modalContent}>
-          <div className={styles.matrixPicker}>
-        
-            <div className={styles.scrollPanel}>
-              {configMaterialSet.map((material, index) => (
-                <>
-                  <div
-                    onClick={() => previewMaterialConfigHandler(index)}
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                    }}
-                  >
-                    <h4>{material.name}</h4>
-                    <div style={{ width: 280 }}>
-                      <PreviewMatrix
-                        key={index + material.simulationDimension}
-                        simulationDimension={material.simulationDimension}
-                        materialMatrix={material.materialMatrix}
-                        srcPositionRelativeX={srcPositionRelativeX}
-                        srcPositionRelativeY={srcPositionRelativeY}
-                      />
-                    </div>
-                  </div>
-                  <hr />
-                </>
-              ))}
-            </div>
-          </div>
-          <div className={styles.editor}>
-
-            {/* Editor start */}
-            
-            <EditorCanvas
-              width={400}
-              height={400}
-              currentMaterial={currentMaterial}
-              srcPositionRelativeX={srcPositionRelativeX}
-              srcPositionRelativeY={srcPositionRelativeY}
-            />
-            {/* Editor end */}
-
-            <div className={styles.buttons}>
-              <WithLabel labelText="Choose matrix size:">
-                <ButtonGroup activeButton={currentMatrixSizeIndex}>
-                  {currentSimulationDimension ===
-                  SimulationDimension.SIMULATION_1D
-                    ? gridSizes1D.map((size, index) => {
-                        return (
-                          <button
-                            key={size}
-                            onClick={() => {
-                              setCurrentMatrixSizeIndex(index);
-                              dispatch(
-                                setMaterialMatrixSize({
-                                  newCountRow: 1,
-                                  newCountCol: size,
-                                })
-                              );
-                              dispatch(
-                                setCurrentMaterialMatrix({
-                                  currentMaterialMatrixConfigInSet,
-                                })
-                              );
-                            }}
-                          >
-                            {size}
-                          </button>
-                        );
-                      })
-                    : gridSizes2D.map((size, index) => {
-                        return (
-                          <button
-                            key={size}
-                            onClick={() => {
-                              setCurrentMatrixSizeIndex(index);
-                              dispatch(
-                                setMaterialMatrixSize({
-                                  newCountRow: size,
-                                  newCountCol: size,
-                                })
-                              );
-                              dispatch(
-                                setCurrentMaterialMatrix({
-                                  currentMaterialMatrixConfigInSet,
-                                })
-                              );
-                            }}
-                          >
-                            {size}
-                          </button>
-                        );
-                      })}
-                </ButtonGroup>
-              </WithLabel>
-              <hr />
-
-              <Button
-                style={{ marginTop: ".61rem" }}
-                onClick={() => setIsOpened(false)}
-              >
-                Back to Simulation
-              </Button>
-            </div>
-          </div>
-
-          <div className={styles.materialPicker}>
-
-
-            <div className={styles.scrollPanel}>
-              {materials.map((material, index) => {
-                return (
+      <Modal>
+        <div className={styles.nonScrollableContent}>
+          {/* <p>This short title <b>IS NOT</b> scrollable</p> */}
+          <h2 className={styles.matrixPickerTitle}>Matrix picker</h2>
+          <h2 className={styles.editorTitle}>Editor</h2>
+          <h2 className={styles.materialPickerTitle}>Material picker</h2>
+        </div>
+        <div className={styles.scrollableContent}>
+          <div className={styles.modalContent}>
+            <div className={styles.matrixPicker}>
+              <div className={styles.scrollPanel}>
+                {configMaterialSet.map((material, index) => (
                   <>
-                    <div className={styles.oneMaterialPanel} key={index}>
-                      <div className={styles.material}>
-                        <svg
-                          onClick={() => setCurrentMaterial(index)}
-                          width={"65px"}
-                          height={"65px"}
-                          style={{
-                            background: colors[index],
-                            border: `${
-                              index == currentMaterial ? "8" : "0"
-                            }px solid rgba(0,0,0,0.35)`,
-                          }}
-                        />
-                        <h3>{material.name}</h3>
-                      </div>
-                      <hr />
-                      <div className={styles.materialNumberInputs}>
-                        <NumberInput
-                          value={material.eps}
-                          label={"permittivity"}
-                          onChange={(e) =>
-                            dispatch(
-                              updateMaterialEps({
-                                materialId: material.id,
-                                newEps: +e.target.value,
-                              })
-                            )
-                          }
-                        />
-                        <NumberInput
-                          value={material.mu}
-                          label={"permiability"}
-                          onChange={(e) =>
-                            dispatch(
-                              updateMaterialMu({
-                                materialId: material.id,
-                                newMu: +e.target.value,
-                              })
-                            )
-                          }
-                        />
-                        <NumberInput
-                          value={material.sigma}
-                          label={"conductivity"}
-                          onChange={(e) => {
-                            dispatch(
-                              updateMaterialSigma({
-                                materialId: material.id,
-                                newSigma: +e.target.value,
-                              })
-                            );
-                          }}
+                    <div
+                      onClick={() => previewMaterialConfigHandler(index)}
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <h4>{material.name}</h4>
+                      <div style={{ width: 280 }}>
+                        <PreviewMatrix
+                          key={index + material.simulationDimension}
+                          simulationDimension={material.simulationDimension}
+                          materialMatrix={material.materialMatrix}
+                          srcPositionRelativeX={srcPositionRelativeX}
+                          srcPositionRelativeY={srcPositionRelativeY}
                         />
                       </div>
-                      <hr />
-                      <hr />
-                      <hr />
                     </div>
+                    <Divider />
                   </>
-                );
-              })}
+                ))}
+              </div>
             </div>
+            <div className={styles.editor}>
+              {/* Editor start */}
+
+              <EditorCanvas
+                width={400}
+                height={400}
+                currentMaterial={currentMaterial}
+                srcPositionRelativeX={srcPositionRelativeX}
+                srcPositionRelativeY={srcPositionRelativeY}
+              />
+              {/* Editor end */}
+
+              <div className={styles.buttons}>
+                <WithLabel labelText="Choose matrix size:">
+                  <ButtonGroup activeButton={currentMatrixSizeIndex}>
+                    {currentSimulationDimension ===
+                    SimulationDimension.SIMULATION_1D
+                      ? gridSizes1D.map((size, index) => {
+                          return (
+                            <button
+                              key={size}
+                              onClick={() => {
+                                setCurrentMatrixSizeIndex(index);
+                                dispatch(
+                                  setMaterialMatrixSize({
+                                    newCountRow: 1,
+                                    newCountCol: size,
+                                  })
+                                );
+                                dispatch(
+                                  setCurrentMaterialMatrix({
+                                    currentMaterialMatrixConfigInSet,
+                                  })
+                                );
+                              }}
+                            >
+                              {size}
+                            </button>
+                          );
+                        })
+                      : gridSizes2D.map((size, index) => {
+                          return (
+                            <button
+                              key={size}
+                              onClick={() => {
+                                setCurrentMatrixSizeIndex(index);
+                                dispatch(
+                                  setMaterialMatrixSize({
+                                    newCountRow: size,
+                                    newCountCol: size,
+                                  })
+                                );
+                                dispatch(
+                                  setCurrentMaterialMatrix({
+                                    currentMaterialMatrixConfigInSet,
+                                  })
+                                );
+                              }}
+                            >
+                              {size}
+                            </button>
+                          );
+                        })}
+                  </ButtonGroup>
+                </WithLabel>
+                <Divider />
+
+                <Button
+                  style={{ marginTop: ".61rem" }}
+                  onClick={() => setIsOpened(false)}
+                >
+                  Back to Simulation
+                </Button>
+              </div>
+            </div>
+
+            <div className={styles.materialPicker}>
+              <div className={styles.scrollPanel}>
+                {materials.map((material, index) => {
+                  return (
+                    <>
+                      <div className={styles.oneMaterialPanel} key={index}>
+                        <div className={styles.material}>
+                          <svg
+                            onClick={() => setCurrentMaterial(index)}
+                            width={"65px"}
+                            height={"65px"}
+                            style={{
+                              background: colors[index],
+                              border: `${
+                                index == currentMaterial ? "8" : "0"
+                              }px solid rgba(0,0,0,0.35)`,
+                            }}
+                          />
+                          <h3>{material.name}</h3>
+                        </div>
+                        <Divider />
+                        <div className={styles.materialNumberInputs}>
+                          <NumberInput
+                            value={material.eps}
+                            label={"permittivity"}
+                            onChange={(e) =>
+                              dispatch(
+                                updateMaterialEps({
+                                  materialId: material.id,
+                                  newEps: +e.target.value,
+                                })
+                              )
+                            }
+                          />
+                          <NumberInput
+                            value={material.mu}
+                            label={"permiability"}
+                            onChange={(e) =>
+                              dispatch(
+                                updateMaterialMu({
+                                  materialId: material.id,
+                                  newMu: +e.target.value,
+                                })
+                              )
+                            }
+                          />
+                          <NumberInput
+                            value={material.sigma}
+                            label={"conductivity"}
+                            onChange={(e) => {
+                              dispatch(
+                                updateMaterialSigma({
+                                  materialId: material.id,
+                                  newSigma: +e.target.value,
+                                })
+                              );
+                            }}
+                          />
+                        </div>
+                        <Divider />
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </div>
+            {/* <DragAndDrop WIDTH={400} HEIGHT={400} /> */}
           </div>
-          {/* <DragAndDrop WIDTH={400} HEIGHT={400} /> */}
         </div>
-        </div>
-        </Modal>
-      {/* </div> */}
+      </Modal>
     </>
   );
 };
