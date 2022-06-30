3d73bf0b5ebf2951d324fad5dc8ce51c30c457e8
max99xam <pepetrov123@list.ru>
2022-06-30 23:42:17 +0300

    refactored Paper component

--- src/components/atoms/paper/Paper.tsx
+++ src/components/atoms/paper/Paper.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styles from "./Paper.module.scss";
 import { PaperProps } from "./Paper.props";
 
-const Paper: React.FC<PaperProps> = ({ children }) => {
+const Paper = ({ children }: PaperProps): JSX.Element => {
   return <div className={styles.paper}>{children}</div>;
 };
 
